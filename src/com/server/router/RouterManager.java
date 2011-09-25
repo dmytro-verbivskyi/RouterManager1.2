47ca5639cb9050c61d03d691323b00b5687885a3
Dima Verbovskiy <dverbovskiy@gmail.com>
2011-09-25 20:27:00 +0300

    Class RouterManager created

---
+++ src/com/server/router/RouterManager.java
@@ -0,0 +1,32 @@
+package com.server.router;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.server.packages.Package;
+
+public class RouterManager {
+    
+    public static void main(String[] args) {
+        Package pack1 = new Package(new StringBuffer("small task 1"));
+        Package pack2 = new Package(new StringBuffer("small task 2"));
+        Package pack3 = new Package(
+                new StringBuffer("very big and huge task 1"));
+        
+        System.out.println("Starting Executor");
+        
+        // create ExecutorService to manage threads
+        ExecutorService threadExecutor = Executors.newCachedThreadPool();
+        
+        // start threads and place in runnable state
+        threadExecutor.execute(pack1);
+        threadExecutor.execute(pack2);
+        threadExecutor.execute(pack3);
+        
+        // shut down worker threads when their tasks complete
+        threadExecutor.shutdown();
+        
+        System.out.println("main ends.\n");
+    }
+    
+}
